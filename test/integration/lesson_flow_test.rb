5978a3fe7471a23066af33c2bb8a6405c6b91f5d
justinkim2015 <justinkim2015@gmail.com>
2022-09-15 14:59:57 +0900

    Update to current

--- test/integration/lesson_flow_test.rb
+++ test/integration/lesson_flow_test.rb
@@ -8,4 +8,18 @@ class LessonFlowTest < ActionDispatch::IntegrationTest
     get "/"
     assert_select "h1"
   end
+
+  test "can create a new lesson" do
+    get "/lessons/new"
+    assert_response :success
+
+    post "/lessons",
+      params: { lesson: { rank: "3", time: "16:54", date: "2022-09-11"} }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "p"
+  end
 end
+
+
