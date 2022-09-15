4cdf35383d587cf6ed2e9e515af14307333de124
justinkim2015 <justinkim2015@gmail.com>
2022-09-15 14:50:04 +0900

    Configure devise to work with minitest

--- test/integration/lesson_flow_test.rb
+++ test/integration/lesson_flow_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class LessonFlowTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "can see the home page" do
+    sign_in users(:one)
     get "/"
     assert_select "h1"
   end
